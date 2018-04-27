39e4bf78036bf6758f2fd7957ad3a8053651e625
Payam <thisispayam@gmail.com>
2018-04-27 00:07:30 -0400

    added the photowall and photo components

---
+++ src/Components/Photowall.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Photo from './Photo';
+
+class Photowall extends Component {
+    render(){
+        return (
+            <div>
+                {this.props.posts.map((post, index) => <Photo key={index} post={post}/>) }
+            </div>
+        )
+    }
+}
+
+export default Photowall;
