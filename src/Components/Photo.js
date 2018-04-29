b6351b6ca92ddd500134d6a34b5b5f2313e1c5d8
Payam <thisispayam@gmail.com>
2018-04-28 20:30:38 -0400

    prop types

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Photo(props){
     const post = props.post;
@@ -15,5 +16,8 @@ function Photo(props){
             </figure>
 }
 
-
+Photo.PropTypes = {
+    post: PropTypes.object.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
 export default Photo;
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Photo from './Photo';
-
+import PropTypes from 'prop-types';
 function PhotoWall(props){
     return (
         <div className='photo-grid'>
            { props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>) } {/*accessing this method coming from App.js in all the photos */}
         </div>
     )
+}
+
+//installed prop-types --save, to use them
+PhotoWall.PropTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
 }
 
 export default PhotoWall;
