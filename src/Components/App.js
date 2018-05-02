ef3301cc9c2670f0b47ec47c5a4fd43d2ecef64b
Payam <pkazemi@vacv.com>
2018-05-02 12:28:18 -0400

    Changed the photos

--- src/Components/App.js
+++ src/Components/App.js
@@ -12,18 +12,24 @@ class App extends Component {
     this.state= {
       posts: [{
           id: 0,
-          description: "beautiful landscape",
-          imageUrl: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-          "3919321_1443393332_n.jpg"
+          description: "Temple!",
+          imageUrl: "https://payam.ca/img/portfolio/photo/2.jpg"
         }, {
           id: 1,
-          description: "Aliens???",
-          imageUrl: "https://img.purch.com/rc/640x415/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3Mi84NTEvb3JpZ2luYWwvc3BhY2V4LWlyaWRpdW00LWxhdW5jaC10YXJpcS1tYWxpay5qcGc=" +
-          "08323785_735653395_n.jpg"
+          description: "Tokyo Tower View!!",
+          imageUrl: "https://payam.ca/img/portfolio/photo/5.jpg"
         }, {
           id: 2,
-          description: "On a vacation!",
-          imageUrl: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+          description: "Monkeys on vacation!",
+          imageUrl: "https://payam.ca/img/portfolio/photo/6.jpg"
+        }, {
+          id: 3,
+          description: "Swimming!",
+          imageUrl: "https://payam.ca/img/portfolio/photo/7.jpg"
+        }, {
+          id: 4,
+          description: "Mr. Lizard",
+          imageUrl: "https://payam.ca/img/portfolio/photo/8.jpg"
         }]
     }
     this.removePhoto = this.removePhoto.bind(this);
